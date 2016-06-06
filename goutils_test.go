44f1ea3f8bd9dbec828ea3a7bbd40adbc823dae8
rykugur <rollhax@gmail.com>
2016-06-05 20:58:56 -0500

    Fix test.

--- goutils_test.go
+++ goutils_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestGetFuncInfo(t *testing.T) {
 	funcInfo := GetFuncInfo()
 
-	if funcInfo != "github.com/rykugur/utils/goutils.TestGetFuncInfo" {
+	if funcInfo != "github.com/rykugur/goutils.TestGetFuncInfo" {
 		t.Error("expected=github.com/rykugur/goutils.TestGetFuncInfo, actual=", funcInfo)
 	}
 }
@@ -13,7 +13,7 @@ func TestGetFuncInfo(t *testing.T) {
 func TestGetPackageName(t *testing.T) {
 	packageName := GetPackageName()
 
-	if packageName != "github.com/rykugur/utils/goutils" {
+	if packageName != "github.com/rykugur/goutils" {
 		t.Error("expected=github.com/rykugur/goutils, actual=", packageName)
 	}
 }
