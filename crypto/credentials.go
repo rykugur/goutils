c64d38bbff8bbb87f7ad4679c8266aa9077d8ec5
rykugur <rollhax@gmail.com>
2016-06-10 23:56:06 -0500

    Added credentials util

---
+++ crypto/credentials.go
@@ -0,0 +1,27 @@
+package credentials
+
+import (
+	log "github.com/Sirupsen/logrus"
+
+	"bufio"
+	"fmt"
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
+	"strings"
+	"syscall"
+)
+
+// Returns the user-supplied username, CLEARTEXT password, and any error
+// that may have occurred.
+func Credentials() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
+	password := string(bytePassword)
+
+	return username, password
+}
